3960f11c1740ed3190ef984d92849f951a02733a
Dergachev Mihail <dergachev.mihail@gmail.com>
2024-10-03 16:01:24 +0300

    font size update

--- src/js/head.js
+++ src/js/head.js
@@ -2,7 +2,7 @@
 (function(){
 	let baseFontSize = Math.max(window.innerWidth / 1920, 1);
 	let vh = window.innerHeight * 0.01;
-	document.documentElement.style.setProperty('--scale', `${baseFontSize}px`);
+	document.documentElement.style.setProperty('--scale', `${1}px`);
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 	const fontLoadEvent = setInterval(() => {
