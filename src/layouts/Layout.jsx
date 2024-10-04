aa4f76459178854889cb035bd801ec5a933318f1
Dergachev Mihail <dergachev.mihail@gmail.com>
2024-10-04 15:05:33 +0300

    add google analytics

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -35,8 +35,16 @@ const Layout = ({ children, title, bodyClass }) => {
 
             <link href={`/assets/styles/styles.css?v=${+new Date()}`} rel="stylesheet" />
             <script src={`/assets/js/head.js?v=${+new Date()}`}></script>
+            <script __html={`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KMZ6M22L');
+            `}></script>
         </head>
         <body data-elt="body" class={bodyClass}>
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMZ6M22L" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <div className="loader"></div>
             <SvgIcons />
             {children}
