5ce5c99f6402c64666678064bd90c05a5484b150
Dergachev Mihail <dergachev.mihail@gmail.com>
2024-10-04 15:02:33 +0300

    update ecosystem and deploy action

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,9 +2,9 @@ module.exports = {
 	apps: [{
 		name: "player-zero",
 		script: "src/server/server.js",
+		node_args: "--import jtsx-loader",
 		watch: false,
 		watch_delay: 1000,
-    	ignore_watch : [".git", "node_modules", "public", "uploads", ".git"],
+    	ignore_watch : [".git", "node_modules", "public", "uploads"]
 	}]
 }
-
